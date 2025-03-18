8352594de09c08ec58f482b3bab34700eae26b19
Elliot Michael Keavney <103308993+Ellwould@users.noreply.github.com>
2025-03-18 07:58:04 +0000

    Update business-calc.go

--- business-calc.go
+++ business-calc.go
@@ -8,6 +8,14 @@ import (
 	"strconv"
 )
 
+var reset = "\033[0m" 
+var red = "\033[31m" 
+var green = "\033[32m" 
+var yellow = "\033[33m" 
+var blue = "\033[34m" 
+var magenta = "\033[35m" 
+var cyan = "\033[36m"
+
 func mainMenu() {
 	fmt.Println("")
 	fmt.Println("   Press any button to continue...")
